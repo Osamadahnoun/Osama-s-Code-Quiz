4fe649928f0add127d4083c71bdd61e5655b654c
Osama Dahnoun <omdahnou@ncsu.edu>
2021-11-04 19:49:47 -0400

    added all five questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ const instr = document.getElementById('instr');
 const title = document.getElementById('title');
 const body = document.getElementById('quiz');
 let score = 0;
+const correct = " Correct! One point was added to your score."
+const incorrect = " Incorrect! 10 Seconds were deducted from your time."
 
 
 
@@ -41,7 +43,7 @@ function questionOne() {
     answerOne.addEventListener('click', hide);
     answerOne.addEventListener('click', function() {
         document.querySelector('footer').innerHTML = "Score: " + score;
-        document.querySelector('footer').append(' Incorrect!'); 
+        document.querySelector('footer').append('. ' + incorrect); 
     })
 
     var answerTwo = document.createElement('button')
@@ -50,7 +52,7 @@ function questionOne() {
     answerTwo.addEventListener('click', hide);
     answerTwo.addEventListener('click', function() {   
         document.querySelector('footer').innerHTML = "Score: " + score;
-        document.querySelector('footer').append(' Incorrect!'); 
+        document.querySelector('footer').append('. ' + incorrect); 
     })
     
     var answerThree = document.createElement('button')
@@ -60,7 +62,7 @@ function questionOne() {
     answerThree.addEventListener('click', function() {
         score++;    
         document.querySelector('footer').innerHTML = "Score: " + score;
-        document.querySelector('footer').append(' Correct!'); 
+        document.querySelector('footer').append('. ' + correct); 
     })
     
     var answerFour = document.createElement('button')
@@ -69,7 +71,7 @@ function questionOne() {
     answerFour.addEventListener('click', hide);
     answerFour.addEventListener('click', function() {
         document.querySelector('footer').innerHTML = "Score: " + score;
-        document.querySelector('footer').append(' Incorrect!'); 
+        document.querySelector('footer').append('. ' + incorrect); 
     })
     
     body.append(questionOne)
@@ -101,7 +103,7 @@ function questionTwo() {
     answerOne.addEventListener('click', hide);
     answerOne.addEventListener('click', function() {
         document.querySelector('footer').innerHTML = "Score: " + score;
-        document.querySelector('footer').append(' Incorrect!'); 
+        document.querySelector('footer').append('. ' + incorrect); 
     })
 
     var answerTwo = document.createElement('button')
@@ -111,7 +113,7 @@ function questionTwo() {
     answerTwo.addEventListener('click', function() { 
         score++;      
         document.querySelector('footer').innerHTML = "Score: " + score;
-        document.querySelector('footer').append(' Correct!')
+        document.querySelector('footer').append('. ' + correct)
     })
     
     var answerThree = document.createElement('button')
@@ -120,7 +122,7 @@ function questionTwo() {
     answerThree.addEventListener('click', hide);
     answerThree.addEventListener('click', function() {
         document.querySelector('footer').innerHTML = "Score: " + score;
-        document.querySelector('footer').append(' Incorrect!'); 
+        document.querySelector('footer').append('. ' + incorrect); 
     })
     
     var answerFour = document.createElement('button')
@@ -129,7 +131,7 @@ function questionTwo() {
     answerFour.addEventListener('click', hide);
     answerFour.addEventListener('click', function() {
         document.querySelector('footer').innerHTML = "Score: " + score;
-        document.querySelector('footer').append(' Incorrect!'); 
+        document.querySelector('footer').append('. ' + incorrect); 
     })
     
     body.append(questionTwo)
@@ -143,60 +145,187 @@ function questionTwo() {
 function questionThree() {
     document.querySelector('footer').innerHTML = "Score: " + score;
     
-    var questionTwo = document.createElement("p");
-    questionTwo.textContent = "The condition in an if/else statement is enclosed with _______________.";
-    questionTwo.setAttribute('style', 'font-size: xx-large')
+    var questionThree = document.createElement("p");
+    questionThree.textContent = "Arrays in Javascript can be used to store ______________.";
+    questionThree.setAttribute('style', 'font-size: xx-large')
 
     function hide() {
-        questionTwo.classList.add('hide');
+        questionThree.classList.add('hide');
         answerOne.classList.add('hide');
         answerTwo.classList.add('hide');
         answerThree.classList.add('hide');
         answerFour.classList.add('hide');
-        // questionTwo();
+        questionFour();
     }
     
     var answerOne = document.createElement('button')
-    answerOne.textContent = "1. quotes";
+    answerOne.textContent = "1. numbers and strings";
     answerOne.setAttribute('class', 'btn');
     answerOne.addEventListener('click', hide);
     answerOne.addEventListener('click', function() {
         document.querySelector('footer').innerHTML = "Score: " + score;
-        document.querySelector('footer').append(' Incorrect!'); 
+        document.querySelector('footer').append('. ' + incorrect); 
     })
 
     var answerTwo = document.createElement('button')
-    answerTwo.textContent = "2. parentheses"
+    answerTwo.textContent = "2. other arrays"
     answerTwo.setAttribute('class', 'btn')
     answerTwo.addEventListener('click', hide);
     answerTwo.addEventListener('click', function() { 
-        score++;      
         document.querySelector('footer').innerHTML = "Score: " + score;
-        document.querySelector('footer').append(' Correct!')
+        document.querySelector('footer').append('. ' + incorrect)
     })
     
     var answerThree = document.createElement('button')
-    answerThree.textContent = "3. curly brackets"
+    answerThree.textContent = "3. booleans"
     answerThree.setAttribute('class', 'btn')
     answerThree.addEventListener('click', hide);
     answerThree.addEventListener('click', function() {
         document.querySelector('footer').innerHTML = "Score: " + score;
-        document.querySelector('footer').append(' Incorrect!'); 
+        document.querySelector('footer').append('. ' + incorrect); 
     })
     
     var answerFour = document.createElement('button')
-    answerFour.textContent = "4. square brackets"
+    answerFour.textContent = "4. all of the above"
     answerFour.setAttribute('class', 'btn')
     answerFour.addEventListener('click', hide);
     answerFour.addEventListener('click', function() {
+        score++;      
         document.querySelector('footer').innerHTML = "Score: " + score;
-        document.querySelector('footer').append(' Incorrect!'); 
+        document.querySelector('footer').append('. ' + correct); 
     })
     
-    body.append(questionTwo)
+    body.append(questionThree)
     body.append(answerOne)
     body.append(answerTwo)
     body.append(answerThree)
     body.append(answerFour)
+
+}
+
+function questionFour() {
+    document.querySelector('footer').innerHTML = "Score: " + score;
+    
+    var questionFour = document.createElement("p");
+    questionFour.textContent = "String values must be enclosed within _________ when being assigned to variables.";
+    questionFour.setAttribute('style', 'font-size: xx-large')
+
+    function hide() {
+        questionFour.classList.add('hide');
+        answerOne.classList.add('hide');
+        answerTwo.classList.add('hide');
+        answerThree.classList.add('hide');
+        answerFour.classList.add('hide');
+        questionFive();
+    }
+    
+    var answerOne = document.createElement('button')
+    answerOne.textContent = "1. commas";
+    answerOne.setAttribute('class', 'btn');
+    answerOne.addEventListener('click', hide);
+    answerOne.addEventListener('click', function() {
+        document.querySelector('footer').innerHTML = "Score: " + score;
+        document.querySelector('footer').append('. ' + incorrect); 
+    })
+
+    var answerTwo = document.createElement('button')
+    answerTwo.textContent = "2. curly brackets"
+    answerTwo.setAttribute('class', 'btn')
+    answerTwo.addEventListener('click', hide);
+    answerTwo.addEventListener('click', function() { 
+        document.querySelector('footer').innerHTML = "Score: " + score;
+        document.querySelector('footer').append('. ' + incorrect)
+    })
+    
+    var answerThree = document.createElement('button')
+    answerThree.textContent = "3. quotes"
+    answerThree.setAttribute('class', 'btn')
+    answerThree.addEventListener('click', hide);
+    answerThree.addEventListener('click', function() {
+        score++; 
+        document.querySelector('footer').innerHTML = "Score: " + score;
+        document.querySelector('footer').append('. ' + correct); 
+    })
+    
+    var answerFour = document.createElement('button')
+    answerFour.textContent = "4. parentheses"
+    answerFour.setAttribute('class', 'btn')
+    answerFour.addEventListener('click', hide);
+    answerFour.addEventListener('click', function() {      
+        document.querySelector('footer').innerHTML = "Score: " + score;
+        document.querySelector('footer').append('. ' + incorrect); 
+    })
+    
+    body.append(questionFour)
+    body.append(answerOne)
+    body.append(answerTwo)
+    body.append(answerThree)
+    body.append(answerFour)
+
+}
+
+function questionFive() {
+    document.querySelector('footer').innerHTML = "Score: " + score;
+    
+    var questionFive = document.createElement("p");
+    questionFive.textContent = "A very useful tool used during development and debugging for printing content to the debugger is:";
+    questionFive.setAttribute('style', 'font-size: xx-large')
+
+    function hide() {
+        questionFive.classList.add('hide');
+        answerOne.classList.add('hide');
+        answerTwo.classList.add('hide');
+        answerThree.classList.add('hide');
+        answerFour.classList.add('hide');
+        // questionFive();
+    }
+    
+    var answerOne = document.createElement('button')
+    answerOne.textContent = "1. JavaScript ";
+    answerOne.setAttribute('class', 'btn');
+    answerOne.addEventListener('click', hide);
+    answerOne.addEventListener('click', function() {
+        document.querySelector('footer').innerHTML = "Score: " + score;
+        document.querySelector('footer').append('. ' + incorrect); 
+    })
+
+    var answerTwo = document.createElement('button')
+    answerTwo.textContent = "2. terminal/bash"
+    answerTwo.setAttribute('class', 'btn')
+    answerTwo.addEventListener('click', hide);
+    answerTwo.addEventListener('click', function() { 
+        document.querySelector('footer').innerHTML = "Score: " + score;
+        document.querySelector('footer').append('. ' + incorrect)
+    })
+    
+    var answerThree = document.createElement('button')
+    answerThree.textContent = "3. for loops"
+    answerThree.setAttribute('class', 'btn')
+    answerThree.addEventListener('click', hide);
+    answerThree.addEventListener('click', function() {
+        document.querySelector('footer').innerHTML = "Score: " + score;
+        document.querySelector('footer').append('. ' + incorrect); 
+    })
+    
+    var answerFour = document.createElement('button')
+    answerFour.textContent = "4. console.log"
+    answerFour.setAttribute('class', 'btn')
+    answerFour.addEventListener('click', hide);
+    answerFour.addEventListener('click', function() {  
+        score++;     
+        document.querySelector('footer').innerHTML = "Score: " + score;
+        document.querySelector('footer').append('. ' + correct); 
+    })
+    
+    body.append(questionFive)
+    body.append(answerOne)
+    body.append(answerTwo)
+    body.append(answerThree)
+    body.append(answerFour)
+
+}
+
+function allDone() {
+    
 
 }
